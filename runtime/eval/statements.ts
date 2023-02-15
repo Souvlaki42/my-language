255db50c9b2888d5e2230fb4d11e0b299c485bb3
Souvlaki42 <souvlaki420@gmail.com>
2023-02-15 14:46:47 +0200

    moving in

---
+++ runtime/eval/statements.ts
@@ -0,0 +1,21 @@
+import { Program, VarDeclaration } from "../../frontend/ast.ts";
+import Environment from "../environment.ts";
+import { evaluate } from "../interpeter.ts";
+import { RuntimeVal, MK_NULL } from "../values.ts";
+
+export function eval_program(program: Program, env: Environment): RuntimeVal {
+	let lastEvaluated: RuntimeVal = MK_NULL();
+
+	for (const statement of program.body) {
+		lastEvaluated = evaluate(statement, env);
+	}
+
+	return lastEvaluated;
+}
+
+export function eval_var_declaration(
+	arg0: VarDeclaration,
+	env: Environment
+): RuntimeVal {
+	throw new Error("Function not implemented.");
+}
