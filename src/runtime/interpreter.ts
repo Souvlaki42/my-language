8203eca4974a17f1bad3b8692969118e4f2540e7
Souvlaki42 <souvlaki420@gmail.com>
2024-04-13 20:14:35 +0300

    Working interpreter for binaries

--- src/runtime/interpreter.ts
+++ src/runtime/interpreter.ts
@@ -1,9 +1,60 @@
 import type { RuntimeVal, NumberVal, NullVal } from "./values.ts";
 import type { Stmt, NumericLiteral, BinaryExpr, Program } from "../ast.ts";
 
-const eval_program = (program: Program): RuntimeVal => {};
+const eval_program = (program: Program): RuntimeVal => {
+let lastEvaluated: RuntimeVal = {
+		value: "null",
+		type: "null",
+	} as NullVal;
 
-const eval_binary_expr = (binop: BinaryExpr): RuntimeVal => {};
+	program.body.forEach((stmt) => {
+		lastEvaluated = evaluate(stmt);
+	});
+
+	return lastEvaluated;
+};
+
+function eval_numeric_binary_expr(left: NumberVal, right: NumberVal, operator: string): NumberVal {
+	let result = 0;
+	if (operator === "+") {
+		result = left.value + right.value;
+	} else if (operator === "-") {
+		result = left.value - right.value;
+	} else if (operator === "*") {
+		result = left.value * right.value;
+	} else if (operator === "/") {
+		if (right.value === 0) {
+			console.error("Division by zero");
+			Deno.exit(1);
+		}
+		result = left.value / right.value;
+	} else if (operator === "%") {
+		result = left.value % right.value;
+	} else {
+		console.error("Invalid operator", operator);
+		Deno.exit(1);
+	}
+
+	return {
+		value: result,
+		type: "number",
+	};
+}
+
+const eval_binary_expr = (binop: BinaryExpr): RuntimeVal => {
+	const leftHandSide = evaluate(binop.left);
+	const rightHandSide = evaluate(binop.right);
+
+	if (leftHandSide.type === "number" && rightHandSide.type === "number") {
+		return eval_numeric_binary_expr(leftHandSide as NumberVal, rightHandSide as NumberVal, binop.operator);
+	}
+
+	// One or both of the operands are null.
+	return {
+		value: "null",
+		type: "null",
+	} as NullVal;
+};
 
 export const evaluate = (astNode: Stmt): RuntimeVal => {
 	switch (astNode.kind) {
